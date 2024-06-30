52c743b0a3f678151896b584bcc587a739173bfc
Carlos Marques <chmarques93@gmail.com>
2024-06-30 18:09:43 -0300

    finished the layout. todo: improve the main activity layout, make use of sqlite db to persist and show notations on main activity

---
+++ app/src/main/java/com/main/mybabyapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.main.mybabyapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fabAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        fabAdd = findViewById(R.id.fab_plus);
+
+        PopupMenu popupMenu = new PopupMenu(this, fabAdd);
+        MenuInflater inflater = popupMenu.getMenuInflater();
+        inflater.inflate(R.menu.plus_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.mama) {
+                    openMamaActivity();
+                    return true;
+                } else if (itemId == R.id.fralda) {
+                    openFraldaActivity();
+                    return true;
+                }
+                return false;
+            }
+        });
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupMenu.show();
+            }
+        });
+    }
+
+    private void openMamaActivity() {
+        Intent intent = new Intent(MainActivity.this, MamaActivity.class);
+        startActivity(intent);
+    }
+
+    private void openFraldaActivity() {
+        Intent intent = new Intent(MainActivity.this, FraldaActivity.class);
+        startActivity(intent);
+    }
+
+}
